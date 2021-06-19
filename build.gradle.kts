09085bb804ebd461ad7f6a157ae77b8191c29593
Matt Glover <mglover@8thlight.com>
2021-06-19 09:04:46 +0100

    Add basic Spring Native app

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+plugins {
+	id("org.springframework.boot") version "2.4.5"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.32"
+	kotlin("plugin.spring") version "1.4.32"
+	id("org.springframework.experimental.aot") version "0.9.2"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	maven { url = uri("https://repo.spring.io/release") }
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("io.r2dbc:r2dbc-postgresql")
+	implementation("am.ik.yavi:yavi:0.5.0")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<BootBuildImage> {
+	builder = "paketobuildpacks/builder:tiny"
+	environment = mapOf("BP_NATIVE_IMAGE" to "true")
+}
---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,154 @@
+package com.example.demo
+
+import am.ik.yavi.builder.ValidatorBuilder
+import am.ik.yavi.builder.konstraint
+import am.ik.yavi.core.ConstraintViolations
+import am.ik.yavi.fn.Either
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
+import io.r2dbc.postgresql.PostgresqlConnectionFactory
+import io.r2dbc.spi.ConnectionFactory
+import java.util.*
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.annotation.Id
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
+import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.data.r2dbc.mapping.R2dbcMappingContext
+import org.springframework.data.relational.core.mapping.NamingStrategy
+import org.springframework.stereotype.Component
+import org.springframework.util.Assert
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyToMono
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@SpringBootApplication class DemoApplication
+
+fun main() {
+  runApplication<DemoApplication>()
+}
+
+class ValidationError(message: String, val constraintViolations: ConstraintViolations) :
+    RuntimeException(message)
+
+interface Validator<T> {
+  fun validationErrorMessage(constraintViolations: ConstraintViolations): String
+
+  fun T.validate(): Either<ConstraintViolations, T>
+
+  fun T.validateToMono(): Mono<T> =
+      validate()
+          .fold(
+              { constraintViolations ->
+                Mono.error(
+                    ValidationError(
+                        validationErrorMessage(constraintViolations), constraintViolations))
+              },
+              { t -> Mono.just(t!!) })
+}
+
+inline fun <reified T : Any> parseAndValidateRequest(
+    request: ServerRequest,
+    validator: Validator<T>
+): Mono<T> = request.bodyToMono<T>().flatMap { t -> validator.run { t.validateToMono() } }
+
+object NewUserValidator : Validator<NewUser> {
+  val validator =
+      ValidatorBuilder.of<NewUser>()
+          .konstraint(NewUser::name) { notBlank().lessThanOrEqual(20) }
+          .konstraint(NewUser::age) { greaterThanOrEqual(21) }
+          .build()
+
+  override fun NewUser.validate(): Either<ConstraintViolations, NewUser> {
+    return validator.validateToEither(this)
+  }
+
+  override fun validationErrorMessage(constraintViolations: ConstraintViolations): String {
+    val violationMessages: String =
+        constraintViolations
+            .details()
+            .map { violationDetail -> violationDetail.defaultMessage }
+            .joinToString(",\n")
+
+    return "NewUser is invalid. The following constraint violations were found:\n" + violationMessages
+  }
+}
+
+data class NewUser(val name: String, val age: Int)
+
+@Configuration
+class GreetRoute {
+  @Bean
+  fun route(userRepository: UserRepository, userHandler: UserHandler) = router {
+    GET("/greet") { _ -> ServerResponse.ok().body(Mono.just("Hello, world!"), String::class.java) }
+    POST("/user", userHandler::addUser)
+    GET("/users", userHandler::fetchUsers)
+  }
+}
+
+@Component
+class UserHandler(private val userRepository: UserRepository) {
+
+  fun addUser(request: ServerRequest): Mono<ServerResponse> =
+      parseAndValidateRequest(request, NewUserValidator)
+          .flatMap(userRepository::add)
+          .flatMap { user -> ServerResponse.ok().bodyValue(user) }
+          .onErrorResume { error ->
+            ServerResponse.badRequest().bodyValue(error.message ?: "Oops!")
+          }
+
+  fun fetchUsers(_request: ServerRequest): Mono<ServerResponse> =
+      ServerResponse.ok().body(userRepository.all(), User::class.java)
+}
+
+@Configuration
+class DatabaseConfiguration : AbstractR2dbcConfiguration() {
+
+  @Bean
+  override fun connectionFactory(): ConnectionFactory {
+    return PostgresqlConnectionFactory(
+        PostgresqlConnectionConfiguration.builder()
+            .host("localhost")
+            .port(5433)
+            .database("devdb")
+            .username("devdb")
+            .password("devpassword")
+            .build())
+  }
+
+  @Bean
+  override fun r2dbcMappingContext(
+      namingStrategy: Optional<NamingStrategy?>,
+      r2dbcCustomConversions: R2dbcCustomConversions
+  ): R2dbcMappingContext {
+    Assert.notNull(namingStrategy, "NamingStrategy must not be null!")
+    val context = R2dbcMappingContext(namingStrategy.orElse(NamingStrategy.INSTANCE)!!)
+    context.setSimpleTypeHolder(r2dbcCustomConversions.simpleTypeHolder)
+    return context
+  }
+}
+
+data class User(@Id val userId: Int, val name: String, val age: Int)
+
+@Component
+class UserRepository(private val r2dbcEntityTemplate: R2dbcEntityTemplate) {
+  fun add(newUser: NewUser): Mono<User> {
+    val (name, age) = newUser
+
+    return r2dbcEntityTemplate
+        .databaseClient
+        .sql("INSERT INTO users(name, age) VALUES (:name, :age) RETURNING user_id")
+        .bind("name", name)
+        .bind("age", age)
+        .map { row -> row.get("user_id", Integer::class.java)!!.toInt() }
+        .one()
+        .map { userId -> User(userId, name, age) }
+  }
+
+  fun all(): Flux<User> = r2dbcEntityTemplate.select(User::class.java).from("users").all()
+}
